eb53574096580ba829cb18e8ab379b76c87796ce
Donnersten <hampus@donnersten.se>
2025-05-19 14:15:32 +0200

    first graph imported to dashboard

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ from frontend.pages.page_4 import page_4
 pages = {"home": home_page, "page_1": page_1, "page_2": page_2, "page_3": page_3, "page_4": page_4,}
 
 Gui(pages=pages).run(
-    dark_mode=False, use_reloader=True, port=8080
+    dark_mode=False, use_reloader=True, port="auto"
 )
