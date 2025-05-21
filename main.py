20bc3b6ff478cdf7d5f9a32ee3f77d2c6f91ede0
Donnersten <hampus@donnersten.se>
2025-05-21 15:12:13 +0200

    styling and kpi's for pange 1 and 4

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from frontend.pages.page_2 import page_2
 from frontend.pages.page_3 import page_3
 from frontend.pages.page_4 import page_4
 
-pages = {"home": home_page, "page_1": page_1, "Program": page_2, "page_3": page_3, "Anordnare": page_4,}
+pages = {"home": home_page, "Kurser": page_1, "Program": page_2, "Studerande": page_3, "Anordnare": page_4,}
 
-Gui(pages=pages).run(
+Gui(pages=pages,css_file="style.css").run(
     dark_mode=False, use_reloader=True, port="auto"
 )
