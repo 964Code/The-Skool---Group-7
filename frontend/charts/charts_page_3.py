6a622b68a0dbff1d4f6e05f9a30ce4afd2ad819b
964Code <964code@gmail.com>
2025-05-25 11:26:51 +0200

    added avg as a temp solution for amount of studying

--- frontend/charts/charts_page_3.py
+++ frontend/charts/charts_page_3.py
@@ -1,27 +1,39 @@
 import plotly.express as px
+import pandas as pd
 
 
 def line_plot_stud(df, areas):
     df_melted = df.melt(id_vars=["År"], var_name="Inriktning", value_name="Studerande")
     resultat = df_melted[df_melted["Inriktning"].isin(areas)]
 
-    # Input list for multiple options
+    avg_per_year = (
+        resultat.groupby("År")["Studerande"]
+        .mean()
+        .reset_index()
+        .rename(columns={"Studerande": "Studerande"})
+    )
+    avg_per_year["Inriktning"] = "Genomsnitt"
+
+
+    resultat_with_avg = pd.concat([resultat, avg_per_year], ignore_index=True)
+
     fig = px.line(
-        resultat,
+        resultat_with_avg,
         x="År",
         y="Studerande",
         color="Inriktning",
-        title=f"Studerande över tid per utbildningsinriktning ",
+        title="Studerande över tid per utbildningsinriktning",
         markers=True,
     )
-    fig.update_traces(line=dict(width= 4, dash="dot"), marker=dict(symbol="circle", size=8))
+
+    fig.update_traces(line=dict(width=4, dash="dot"), marker=dict(symbol="circle", size=8))
 
     fig.update_layout(
-        plot_bgcolor = "white",
-        title_font=dict(family="Times New Roman", size=30, weight="bold", color="black",),
-        font=dict(family="Times New Roman", size=15, color="black",),
+        plot_bgcolor="white",
+        title_font=dict(family="Times New Roman", size=30, weight="bold", color="black"),
+        font=dict(family="Times New Roman", size=15, color="black"),
         yaxis_tickfont=dict(family="Times New Roman", size=15, weight="bold", color="black"),
-        xaxis_tickfont=dict(family="Times New Roman", size=15, weight="bold", color="black")
-        )
+        xaxis_tickfont=dict(family="Times New Roman", size=15, weight="bold", color="black"),
+    )
     return fig
 
